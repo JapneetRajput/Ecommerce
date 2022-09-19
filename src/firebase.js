2114347ac68e650117780dacd7b8fe90cca43be3
Japneet Rajput <japneetrajput2804@gmail.com>
2022-09-19 20:20:47 +0530

    Firebase config

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAJS-TscGgZgvvLqDp0UojfHEeyq6iD72o",
+  authDomain: "electronix-1f08b.firebaseapp.com",
+  projectId: "electronix-1f08b",
+  storageBucket: "electronix-1f08b.appspot.com",
+  messagingSenderId: "733925107864",
+  appId: "1:733925107864:web:24e774d2f4a9cba1d188ed",
+  measurementId: "G-5J2F1M1Y8J"
+};
+
+const app = initializeApp(firebaseConfig);
