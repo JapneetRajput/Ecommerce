11d3f25765c0c28cff56f918506cd531bc3dd4e8
maithilibrid <yashbrid03@gmail.com>
2022-09-19 20:36:49 +0530

    routes, Home page w Carousel

--- src/components/BSCarousel.js
+++ src/components/BSCarousel.js
@@ -1,5 +1,8 @@
 import React from "react";
 import fourth from "../images/fourth.png";
+import img_first from "../images/img_first.png";
+import img_third from "../images/img_third.png";
+import img_second from "../images/img_second.png";
 import { Carousel } from "react-bootstrap";
 export default function BSCarousel() {
   return (
@@ -9,13 +12,13 @@ export default function BSCarousel() {
           <div className="col-12">
             <Carousel interval="3000">
               <Carousel.Item>
-                <img className=" w-100" src={fourth} alt="First slide" />
+                <img className=" w-100" src={img_first} alt="First slide" />
               </Carousel.Item>
               <Carousel.Item>
-                <img className=" w-100" src={fourth} alt="Second slide" />
+                <img className=" w-100" src={img_second} alt="Second slide" />
               </Carousel.Item>
               <Carousel.Item>
-                <img className=" w-100" src={fourth} alt="Third slide" />
+                <img className=" w-100" src={img_third} alt="Third slide" />
               </Carousel.Item>
             </Carousel>
           </div>
